05d892fb9ad40269fe04d5cbadc15ec30d2b3b16
Matt Strong <mastro1@stanford.edu>
2023-12-06 18:19:26 -0800

    in progress nerfstudio data

---
+++ train_ds_nerf_example.py
@@ -0,0 +1,10 @@
+
+
+if __name__ == '__main__':
+    # with available depth, uncertainty, and rgb images, train a nerf model
+    
+    # load data into a format nerfstudio can use
+    
+    
+    # train the nerf 
+    pass
